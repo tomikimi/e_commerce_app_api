8b032d3ca415de19ef3c390b9030f0e874198fed
Tomikimi <tomikimiogoli@yahoo.com>
2026-09-16 16:57:30 +0100

    setup of user API endpoint

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const categoryRouter = require("./routes/category");
+const userRouter = require("./routes/users");
 const app = express();
 
 app.use(express.json());
 
 app.use("/category", categoryRouter);
+app.use("/users", userRouter);
 
 module.exports = { app };
--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,95 @@
 const express = require("express");
 const pool = require("../db/index");
+const hashPassword = require("../util/hashPassword");
 const userRoute = express.Router();
 
-userRoute.get("/user", (req, res) => {
-  console.log(res);
-});
-
-userRoute.post("/user", async (req, res) => {
+userRoute.get("/getUser/:id", async (req, res) => {
   try {
-    const {} = req.body();
-    const response = await pool.query();
+    const userID = parseInt(req.params.id);
+    const response = await pool.query(
+      `SELECT PUBLIC.SELECT_USER_FUNCTION($1)`,
+      [userID],
+    );
+
+    const result = response.rows[0].select_user_function;
+
+    if (result.SUCCESS === true) {
+      res.status(200).json(result);
+    } else {
+      res.status(400).json(result);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+userRoute.post("/postUser", hashPassword, async (req, res) => {
+  try {
+    const { firstName, lastName, email, phoneNumber, homeAddress, state } =
+      req.body;
+    const userPassword = req.passwordHash;
+
+    const response = await pool.query(
+      `SELECT PUBLIC.INSERT_USERS_FUNC($1,$2,$3,$4,$5,$6,$7)`,
+      [
+        firstName,
+        lastName,
+        email,
+        phoneNumber,
+        homeAddress,
+        state,
+        userPassword,
+      ],
+    );
+
+    const result = response.rows[0].insert_users_func;
+
+    if (result.SUCCESS === true) {
+      res.status(201).json(result);
+    } else {
+      res.status(400).json(result);
+    }
   } catch (error) {}
-
-  console.log(res);
 });
 
-userRoute.put("/user/:id", (req, res) => {
-  console.log(res);
+userRoute.put("/updateUser/:id", async (req, res) => {
+  try {
+    const { firstName, lastName, phoneNumber, homeAddress, state } = req.body;
+    const userID = parseInt(req.params.id);
+
+    const response = await pool.query(
+      `SELECT PUBLIC.UPDATE_USER_FUNCTION($1,$2,$3,$4,$5,$6)`,
+      [userID, firstName, lastName, phoneNumber, homeAddress, state],
+    );
+
+    const result = response.rows[0].update_user_function;
+    if (result.SUCCESS === true) {
+      res.status(200).json(result);
+    } else {
+      res.status(400).json(result);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-userRoute.delete("/user/:id", (req, res) => {
-  console.log(res);
+userRoute.delete("/deleteUser/:id", async (req, res) => {
+  try {
+    const userID = parseInt(req.params.id);
+    const response = await pool.query(
+      `SELECT PUBLIC.DELETE_USERS_FUNCTION($1)`,
+      [userID],
+    );
+
+    const result = response.rows[0].delete_users_function;
+    if (result.SUCCESS === true) {
+      res.status(200).json(result);
+    } else {
+      res.status(400).json(result);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = userRoute;
